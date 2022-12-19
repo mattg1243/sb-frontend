adf6b5fca9ee90b5679406dccf16b4fc930a8eeb
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-18 22:07:55 -0800

    improved DashRow component

--- src/components/DashRow.tsx
+++ src/components/DashRow.tsx
@@ -1,21 +1,29 @@
-import { Card, Space, Image } from 'antd';
+import { Card, Space, Image, Button } from 'antd';
 import ReactAudioPlayer from 'react-audio-player';
 import logo from '../assets/logo_four_squares.png';
+import { PlusOutlined } from '@ant-design/icons';
 
-export default function DashRow(props: { beat: any }): JSX.Element {
-  const beat = props.beat;
+interface IBeatRowProps {
+  beat: any,
+  onClick: Function,
+}
+
+export default function DashRow(props: IBeatRowProps): JSX.Element {
+  const { beat, onClick } = props;
 
   return (
-    <div className="space-align-container">
-      <Space direction="vertical" size="middle">
+    <div>
+      <Space direction="vertical" size="middle" style={{ width: '100%' }}>
         <Space direction="horizontal">
-          <Image src={logo} alt="album artwork" width="8rem" style={{ border: 'solid' }} />
+          <Button type='ghost' size='large'>
+            <PlusOutlined />
+          </Button>
+          <Image src={logo} alt="album artwork" width="8rem" />
           <Space direction="vertical" size="small" style={{ display: 'flex' }}>
-            <Card style={{ background: 'var(--primary', borderRadius: '16px', borderColor: 'black', margin: '1rem' }}>
+            <Card style={{ background: 'var(--primary', border: 'none' }}>
               <Space direction="horizontal" align="start">
                 <Space direction="vertical">
-                  <h3>Devil in a New Dress - Kanye West</h3>
-                  <p>3:26</p>
+                  <h3 onClick={() => {onClick()}} style={{ cursor: 'pointer' }}>Devil in a New Dress - Kanye West</h3>
                 </Space>
                 <Space align="end" />
                 <p>Hip-Hop & Rap | 145bpm</p>
