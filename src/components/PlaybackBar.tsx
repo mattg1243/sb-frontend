d844575efb0f833f32219d534a34279044e35aa4
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-18 22:14:46 -0800

    created a ployback footer component, still in a rough state

---
+++ src/components/PlaybackBar.tsx
@@ -0,0 +1,43 @@
+import { Footer } from 'antd/es/layout/layout';
+import ReactAudioPlayer from 'react-audio-player';
+
+interface IPlayBackBarProps {
+  trackTitle: string, 
+  trackArtist: string,
+  trackSrcUrl: string,
+  isShown: boolean,
+}
+
+export default function PlayBackBar(props: IPlayBackBarProps) {
+
+  const {trackTitle, trackArtist, trackSrcUrl, isShown } = props;
+
+  return (
+    <>
+    {
+    isShown ? 
+    <Footer 
+      style={{ 
+          position: 'fixed', 
+          left: 0, 
+          bottom: 0, 
+          width: '100%', 
+          textAlign: 'center', 
+          background: 'black', 
+          color: 'white' 
+        }}
+        >
+      <p>{trackTitle}</p>
+      <p>{trackArtist}</p>
+      <ReactAudioPlayer 
+        src={trackSrcUrl}
+        style={{ color: 'white', background: 'white'}} 
+        controls={true}
+        autoPlay={true}
+      />
+    </Footer> :
+    null
+    }
+    </>
+  )
+}
