3e26519a1d923a22a52e91e08a3602eae303d0b3
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-18 22:09:00 -0800

    removed Content wrapper around child component in BaseLayout component

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -10,9 +10,7 @@ export default function BaseLayout(props: BaseLayoutProps) {
 
   return (
     <Layout style={{ width: '100%', height: '100%' }}>
-      <Content style={{ padding: '50px', width: '100%', justifyContent: 'center' }}>
         <Component />
-      </Content>
     </Layout>
   );
 }
