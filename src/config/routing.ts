6720a859120649bd737a022653a98f437930303b
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-22 16:52:04 -0800

    routing config

---
+++ src/config/routing.ts
@@ -0,0 +1,10 @@
+const devGatewayUrl = 'http://localhost:8000'
+const prodGatewayUrl = 'https://sb-gateway.onrender.com'
+
+let gatewayUrl: string;
+
+process.env.NODE_ENV === 'production' ? gatewayUrl = prodGatewayUrl: gatewayUrl = devGatewayUrl;
+
+export default gatewayUrl;
+
+export const cdnHostname = 'https://d3fulr0i8qqtgb.cloudfront.net';
