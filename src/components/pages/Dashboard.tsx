f3e47b17bb83cda4382dc1b5776f9f7897ffbe61
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-22 16:50:12 -0800

    added logic for displaying loading spinner, using hook to get beats

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,32 +1,31 @@
 import { Content } from 'antd/lib/layout/layout';
 import DashRow from '../DashRow';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
 import PlayBackBar from '../PlaybackBar';
+import useGetBeats from '../../hooks/useGetBeats';
+import { Spin } from 'antd';
+import { cdnHostname } from '../../config/routing';
 
 export default function Dashboard() {
   // TODO: this should hold the actual beat obj to play or null when none are playing
-  const [trackPlaying, setTrackPlaying] = useState<boolean>(false);
-  const [beats, getBeats] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [trackPlaying, setTrackPlaying] = useState<string>();
 
-  const testClick = () => { console.log('click registered') }
-
-  const topTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const { beats } = useGetBeats();
 
   return (
     <>
       <Navbar />
-        <Content style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
-          {topTen.map((beat) => {
-            return <DashRow beat={beat}  onClick={()=>{setTrackPlaying(true)}} />;
-          })}
+        <Content style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', height: '100%' }}>
+          { beats ? beats.map((beat) => {
+            return <DashRow beat={beat}  onClick={()=>{setTrackPlaying(`${cdnHostname}/${beat.audioKey}`)}} />;
+          }): <Spin />}
         </Content>
         <PlayBackBar 
           trackTitle="Devil in a New Dress" 
           trackArtist='Kanye West' 
-          trackSrcUrl='https://d3fulr0i8qqtgb.cloudfront.net/beats/c5127337c7cf68db6697f6345663f4e7'
-          isShown={trackPlaying}
+          trackSrcUrl={trackPlaying ? trackPlaying: ''}
+          isShown={trackPlaying !== undefined}
         />
     </>
   );
