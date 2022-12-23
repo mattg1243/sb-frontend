20be372f2ba697bb2e0bccf78ba9a128682f356d
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-22 16:53:21 -0800

    added Beat type that mirrors response from server

---
+++ src/types/beat.ts
@@ -0,0 +1,16 @@
+export type Beat = {
+  _id: string,
+  artistId: string,
+  artistName: string, 
+  artworkKey: string | null,
+  audioKey: string,
+  description: string | null,
+  genreTags: Array<string>,
+  otherTags: Array<string> | null,
+  key: string,
+  licensed: boolean,
+  tempo: number,
+  title: string,
+  created_at: string | Date,
+  updated_at: string | Date,
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 // alerts and errors
 export type { AlertObj } from './alerts';
+// database entities
+export type { Beat } from './beat';
