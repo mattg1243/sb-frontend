64cad7e622890219d85ca5cb29fe5a04be785989
Matt Gallucci <mattgallucci97@gmail.com>
2022-12-31 11:49:27 -0800

    moved axios request to their own module

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -21,6 +21,14 @@ export const sendLoginUserReq = async (credentials: ILoginRequest): Promise<Axio
     )
 }
 
+export const getUser = async (userId: string) => {
+  return await axios.get(`${gatewayUrl}/user/?id=${userId}`, { withCredentials: true });
+}
+
 export const getAllBeatsReq = async () => {
   return await axios.get(`${gatewayUrl}/beats/beats`);
+}
+
+export const getAllBeatsByUserReq = async (userId: string) => {
+  return await axios.get(`${gatewayUrl}/beats/beats?user=${userId}`)
 }
