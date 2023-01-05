49b196f973f96be129202e7b047c7029a9c68ef8
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-04 23:38:59 -0800

    added a generic ModalForm component, not yet being used

---
+++ src/components/ModalForm.tsx
@@ -0,0 +1,36 @@
+import { Modal, Button } from 'antd';
+import { useState } from 'react';
+
+interface IButtonInfo {
+  label: string,
+  onClick: Function
+}
+
+interface IModalFormProps {
+  form: React.ReactNode,
+  saveButton: IButtonInfo,
+}
+
+export default function ModalForm(props: IModalFormProps) {
+  const { form, saveButton } = props;
+  const { label, onClick } = saveButton;
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleCancel = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <Modal
+      footer={[
+        <Button onClick={handleCancel}>Cancel</Button>,
+        <Button onClick={(e) => { onClick() }}>{label}</Button>
+      ]}
+    >
+      {form}
+    </Modal>
+  )
+  
+}
