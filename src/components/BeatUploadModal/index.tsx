0acb88287db87a8d0b48e7f4b4aa0bf338854ac8
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-05 17:51:19 -0800

    added feedback for uploading beat and server response

--- src/components/BeatUploadModal/index.tsx
+++ src/components/BeatUploadModal/index.tsx
@@ -1,14 +1,15 @@
-import { Modal, Form, Button, Input, Select, Radio, RadioChangeEvent, Divider } from 'antd';
+import { Modal, Form, Button, Input, Select, Radio, RadioChangeEvent, Divider, Spin, Alert } from 'antd';
 import { CheckCircleOutlined, PictureOutlined, SoundOutlined } from '@ant-design/icons';
-import { IoIosMusicalNotes } from 'react-icons/io';
 import { useState } from 'react';
-import { uploadBeatReq, getAllBeatsReq } from '../../lib/axios';
+import { uploadBeatReq } from '../../lib/axios';
 import { genreOptions} from '../../utils/genreTags';
 import UploadButton from '../UploadButton';
 
 export default function UploadBeatModal() {
 
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [alert, setAlert] = useState<{ message: string, type: 'error' | 'success' }>();
+  const [isUploading, setIsUploading] = useState<boolean>(false);
   const [title, setTitle] = useState<string>('');
   const [genreTags, setGenreTags] = useState<Array<string>>(['']);
   const [tempo, setTempo] = useState<string>('');
@@ -34,10 +35,19 @@ export default function UploadBeatModal() {
     formData.append('key', key);
 
     try {
+      setIsUploading(true);
       const response = await uploadBeatReq(formData);
+      if (response.status === 200) { 
+        setAlert({ message: 'Your beat was uploaded successfully!', type: 'success'});
+      }
       console.log(response);
-    } catch (err) {
+    } 
+    catch (err) {
+      setAlert({ message: 'There was an error uploading your beat.', type: 'error'})
       console.error(err);
+    }
+    finally {
+      setIsUploading(false);
     }
   }
 
@@ -64,47 +74,53 @@ export default function UploadBeatModal() {
   return (
     <>
       <Button type='ghost' onClick={() => { setShowModal(true) }} style={{ color: 'white' }} >Upload</Button>
+      {alert ?
+       <Alert message={alert.message} type={alert.type} closable showIcon /> :
+       null
+      }
       <Modal title="Upload Your Beat" open={showModal} onCancel={handleCancel} footer={null}>
-          <Form style={{ margin: '2rem 0' }}>
-            <Form.Item>
-              <Input placeholder='Title' onChange={(e) => { setTitle(e.target.value)}}></Input>
-            </Form.Item>
-            <Form.Item>
-              <Select 
-                placeholder="Genre Tags"
-                options={genreOptions}
-                mode='multiple'
-                maxTagCount='responsive'
-                onChange={handleGenreTagsChange}
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input placeholder="Tempo" onChange={(e) => { setTempo(e.target.value)}}></Input>
-            </Form.Item>
-            <Form.Item>
-              <Select
-                placeholder='Key'
-                options={possibleKeyOptions}
-                onChange={(e) => setKey(e.target.value)}
-              />
-              <Radio.Group onChange={(e) => { handleSharpFlatChange(e) }} style={{ marginTop: '.5rem' }}>
-                <Radio value='' checked={flatOrSharp === ''}>None</Radio>
-                <Radio value='b' checked={flatOrSharp === '♭'}>♭</Radio>
-                <Radio value='#' checked={flatOrSharp === '#'}>#</Radio>
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item>
-              <UploadButton label='Artwork Upload' allowedFileType='image/*' uploadStateSetter={setArtwork} sideIcon={<PictureOutlined />} />
-              {artwork ? <CheckCircleOutlined style={{ marginLeft: '1rem', fontSize: '1rem',  }} /> : null}
-            </Form.Item>
-            <Form.Item>
-              <UploadButton label='Beat Upload' allowedFileType='mpeg/wav' uploadStateSetter={setAudio} sideIcon={<SoundOutlined />} />
-              {audio ? <CheckCircleOutlined style={{ marginLeft: '1rem', fontSize: '1rem',  }} /> : null}
-            </Form.Item>
-          </Form>
+          <Spin spinning={isUploading} tip={'Your beat is being uploaded, you can close this window. You will be notified when it is done.'}>
+            <Form style={{ margin: '2rem 0' }}>
+              <Form.Item>
+                <Input placeholder='Title' onChange={(e) => { setTitle(e.target.value)}}></Input>
+              </Form.Item>
+              <Form.Item>
+                <Select 
+                  placeholder="Genre Tags"
+                  options={genreOptions}
+                  mode='multiple'
+                  maxTagCount='responsive'
+                  onChange={handleGenreTagsChange}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Input placeholder="Tempo" onChange={(e) => { setTempo(e.target.value)}}></Input>
+              </Form.Item>
+              <Form.Item>
+                <Select
+                  placeholder='Key'
+                  options={possibleKeyOptions}
+                  onChange={(e) => setKey(e.target.value)}
+                />
+                <Radio.Group onChange={(e) => { handleSharpFlatChange(e) }} style={{ marginTop: '.5rem' }}>
+                  <Radio value='' checked={flatOrSharp === ''}>None</Radio>
+                  <Radio value='b' checked={flatOrSharp === '♭'}>♭</Radio>
+                  <Radio value='#' checked={flatOrSharp === '#'}>#</Radio>
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item>
+                <UploadButton label='Artwork Upload' allowedFileType='image/*' uploadStateSetter={setArtwork} sideIcon={<PictureOutlined />} />
+                {artwork ? <CheckCircleOutlined style={{ marginLeft: '1rem', fontSize: '1rem',  }} /> : null}
+              </Form.Item>
+              <Form.Item>
+                <UploadButton label='Beat Upload' allowedFileType='audio/*' uploadStateSetter={setAudio} sideIcon={<SoundOutlined />} />
+                {audio ? <CheckCircleOutlined style={{ marginLeft: '1rem', fontSize: '1rem',  }} /> : null}
+              </Form.Item>
+            </Form>
+          </Spin>
           <Divider />
-          <Button onClick={() => { setShowModal(false); }} >Cancel</Button>
-          <Button type='primary' style={{ marginLeft: '1rem', backgroundColor: 'var(--primary)', color: 'black' }} onClick={() => { handleSubmit(); }} >Upload Beat</Button>
+          <Button onClick={() => { setShowModal(false); }} disabled={isUploading} >Cancel</Button>
+          <Button type='primary' style={{ marginLeft: '1rem', backgroundColor: 'var(--primary)', color: 'black' }} onClick={() => { handleSubmit(); }} disabled={isUploading} >Upload Beat</Button>
       </Modal>
     </>
 )
