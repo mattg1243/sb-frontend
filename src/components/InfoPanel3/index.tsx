21f185f04ac189e3d5271969be8c4bdf10790e88
TheDakDakDak <thedakdakdak@gmail.com>
2023-02-07 21:38:47 -0800

    Typo fixed. Beat to beats

--- src/components/InfoPanel3/index.tsx
+++ src/components/InfoPanel3/index.tsx
@@ -6,7 +6,7 @@ export default function InfoPanel1() {
   return (
     <Row gutter={0} className={styles.row}>
       <Col span={12}>
-        <h1 className={styles.header1}>Collect royalties on your beat <em>forever</em>.</h1>
+        <h1 className={styles.header1}>Collect royalties on your beats <em>forever</em>.</h1>
         <p className={styles.paragraph1}>When an artist licenses your beat through Sweatshop Beats, you are entitled to 3%-5% royalties <em>forever</em>.</p>
       </Col>
       <Col span={12}>
