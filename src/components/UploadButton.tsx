a104019aa9b80270ff504da57762cad85d2b78a5
Matt Gallucci <mattgallucci97@gmail.com>
2023-02-08 19:58:08 -0800

    added disabled prop to UploadButton to pass through to the base Button

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,18 +1,18 @@
 import { Button } from "antd";
 import React, { useRef } from "react";
-import { PictureOutlined } from "@ant-design/icons";
 
 interface IUploadButtonProps {
   label: string,
   // this audio field needs to be narrowed down to mp3 and wav
   allowedFileType: 'audio/*' | 'image/*'
-  uploadStateSetter: Function
-  sideIcon? : React.ReactNode
+  uploadStateSetter: Function,
+  sideIcon? : React.ReactNode,
+  disabled?: boolean,
 }
 
 export default function UploadButton(props: IUploadButtonProps) {
 
-  const { label, allowedFileType, uploadStateSetter, sideIcon } = props;
+  const { label, allowedFileType, uploadStateSetter, sideIcon, disabled } = props;
 
   const hiddenFileInput = useRef<HTMLInputElement>(null);
 
@@ -24,7 +24,7 @@ export default function UploadButton(props: IUploadButtonProps) {
 
   return (
     <>
-      <Button onClick={handleClick} icon={sideIcon} >
+      <Button onClick={handleClick} icon={sideIcon} disabled={disabled} >
         {label}
       </Button>
       <input type='file' accept={allowedFileType} style={{ display: 'none' }} ref={hiddenFileInput} onChange={(e) => { if (e.target.files) { uploadStateSetter(e.target.files[0]); }} } />
