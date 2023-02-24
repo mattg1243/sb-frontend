df85aa4b248d6165b29ba4736d35addefda6dd26
Matt Gallucci <mattgallucci97@gmail.com>
2023-02-24 14:24:36 -0800

    Profile component test, refactoring

--- src/components/pages/Dashboard/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import Dashboard from './index';
-
-test('Renders', async () => {
-  render(Dashboard());
-  const dashboard = await screen.findByTestId('dashboard');
-  expect(dashboard).toBeInTheDocument()
-  cleanup();
-})
---
+++ src/components/pages/Profile/Profile.test.tsx
@@ -0,0 +1,8 @@
+
+import Profile from "."
+
+describe('Profile.cy.tsx', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
