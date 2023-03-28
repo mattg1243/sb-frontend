d7a9136443d3fae020b1f9571889e5f0e04ddcd7
Matt Gallucci <mattgallucci97@gmail.com>
2023-03-27 22:55:26 -0700

    recomendation algorithm selector on dashboard page

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -4,21 +4,65 @@ import DashRow from '../../DashRow';
 import { useState } from 'react';
 // import Navbar from '../../Navbar';
 import useGetBeats from '../../../hooks/useGetBeats';
-import { Spin } from 'antd';
+import { Button, Dropdown, MenuProps, Spin } from 'antd';
 import { cdnHostname } from '../../../config/routing';
 import { Beat } from '../../../types/beat';
 import PlaybackButtons from '../../PlaybackButtons';
 import styles from './Dashboard.module.css';
+import { DownOutlined } from '@ant-design/icons';
+
+type RecAlgos = 'Following' | 'Recommended';
 
 export default function Dashboard() {
-  // TODO: this should hold the actual beat obj to play or null when none are playing
   const [trackPlaying, setTrackPlaying] = useState<Beat>();
+  const [recAlgo, setRecAlgo] = useState<RecAlgos>('Recommended');
 
   const { beats } = useGetBeats();
+
+  const algoOptions: MenuProps['items'] = [
+    {
+      key: 'Recommended',
+      label: (
+        <Button
+          type="ghost"
+          style={{ color: 'white' }}
+          onClick={() => {
+            if (recAlgo !== 'Recommended') {
+              setRecAlgo('Recommended');
+            }
+          }}
+        >
+          Recommended
+        </Button>
+      ),
+    },
+    {
+      key: 'Following',
+      label: (
+        <Button
+          type="ghost"
+          style={{ color: 'white' }}
+          onClick={() => {
+            if (recAlgo !== 'Following') {
+              setRecAlgo('Following');
+            }
+          }}
+        >
+          Following
+        </Button>
+      ),
+    },
+  ];
 
   return (
     <div data-testid="dashboard" style={{ width: '100%' }}>
       <h2 className={styles['for-you-text']}>For you:</h2>
+      <Dropdown menu={{ items: algoOptions }}>
+        <Button type="ghost" className={styles['algo-btn']}>
+          {recAlgo}
+          <DownOutlined />
+        </Button>
+      </Dropdown>
       <div className={styles['beats-container']}>
         {beats ? (
           beats.map((beat) => {
