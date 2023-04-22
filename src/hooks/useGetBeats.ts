e8f87ef43e5dd8041110df5731e6e968572fb302
Matt Gallucci <mattgallucci97@gmail.com>
2023-04-22 13:31:43 -0700

    updated useGetBeats hook to interface with new API spec

--- src/hooks/useGetBeats.ts
+++ src/hooks/useGetBeats.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import type { Beat } from '../types';
-import { getAllBeatsReq, getAllBeatsByUserReq } from '../lib/axios';
+import { getAllBeatsReq, getAllBeatsByUserReq, getAllBeatsFromFollowingReq } from '../lib/axios';
 
 /**
  * A hook that gets all Beats from the server, if supplied with a user ID, returns all beats created
@@ -8,23 +8,38 @@ import { getAllBeatsReq, getAllBeatsByUserReq } from '../lib/axios';
  * @param userId - Optional param used for querying Beats by user.
  * @returns An array of Beat objects or undefined if no beats match the query.
  */
-export default function useGetBeats(userId?: string): { beats: Array<Beat> | undefined } {
+export default function useGetBeats(
+  userId?: string,
+  following?: boolean
+): { beats: Array<Beat> | undefined; isLoading: boolean } {
   const [beats, setBeats] = useState<Array<Beat>>();
-
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
-    if (!userId) {
-      getAllBeatsReq().then((res) => {
-        setBeats(res.data);
-      });
-    } else {
-      getAllBeatsByUserReq(userId).then((res) => {
-        setBeats(res.data);
-      });
+    setIsLoading(true);
+    console.log('getting beats...');
+    try {
+      if (!userId) {
+        getAllBeatsReq().then((res) => {
+          setBeats(res.data);
+        });
+      } else if (userId && following) {
+        getAllBeatsFromFollowingReq(userId).then((res) => {
+          setBeats(res.data);
+        });
+      } else {
+        getAllBeatsByUserReq(userId).then((res) => {
+          setBeats(res.data);
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+      console.log('done!');
     }
   }, [userId]);
-
   if (beats?.length === 0) {
-    return { beats: undefined };
+    return { beats: undefined, isLoading };
   }
-  return { beats };
+  return { beats, isLoading };
 }
