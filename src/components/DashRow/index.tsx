07b97ceac0a0beb38af5c53e36839bd54e53d51d
Matt Gallucci <mattgallucci97@gmail.com>
2023-06-14 10:19:29 -0700

    mobile beat streaming is now fully functional along with metadata display

--- src/components/DashRow/index.tsx
+++ src/components/DashRow/index.tsx
@@ -3,11 +3,13 @@ import BeatEditModal from '../BeatEditModal';
 import { Beat } from '../../types/beat';
 import { cdnHostname } from '../../config/routing';
 import artworkLoading from '../../assets/artwork_loading.jpg';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import playIcon from '../../assets/play_black.png';
 import pauseIcon from '../../assets/pause_black.png';
 import styles from './DashRow.module.css';
 import BeatDownloadModal from '../BeatDownloadModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { playback } from '../../reducers/playbackReducer';
 
 interface IBeatRowProps {
   beat: Beat;
@@ -20,10 +22,15 @@ const isMobile: boolean = window.innerWidth < 480;
 export default function DashRow(props: IBeatRowProps): JSX.Element {
   const [artistNameColor, setArtistNameColor] = useState<'black' | 'blue'>('black');
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isSelected, setIsSelected] = useState<boolean>(false);
 
   const { beat, onClick, buttonType } = props;
-  // hoist up the scope of the audio var
-  const audio = useRef<HTMLAudioElement | null>(null);
+
+  const dispatch = useDispatch();
+  // TODO figure out why this has to be typed everytime its used
+  const beatPlaying = useSelector<{ playback: { trackPlaying: Beat | null } }, Beat | null>(
+    (state) => state.playback.trackPlaying
+  );
 
   const displayFlatOrSharp = (flatOrSharpStr: 'flat' | 'sharp' | '') => {
     if (flatOrSharpStr === '') {
@@ -35,87 +42,117 @@ export default function DashRow(props: IBeatRowProps): JSX.Element {
     }
   };
 
-  // fn for stopping all audio on mobile
   const stopAllAudio = () => {
-    document.querySelectorAll('audio').forEach((element) => {
-      element.pause();
+    document.querySelectorAll('audio').forEach((el) => {
+      el.pause();
+      el.currentTime = 0;
     });
   };
 
+  /**
+   * Sets the MediaSession metadata for display on mobile devices.
+   */
+  const setAudioMetadata = () => {
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: beat.title,
+      artist: beat.artistName,
+      artwork: [{ src: `${cdnHostname}/${beat.artworkKey}` }],
+    });
+  };
+  /**
+   * This function handles beat playback for mobile and passes through the provided
+   * function from the props if on desktop.
+   * @param e - MouseEvent
+   */
+  const playBeat = (e: React.MouseEvent<HTMLHeadingElement | HTMLDivElement>) => {
+    if (isMobile) {
+      // select the corresponding hidden audio tag form the DOM
+      const audio = document.getElementById(`audio-player-${beat.audioKey}`) as HTMLAudioElement;
+      // another beat is playing, stop it and play this beat
+      if (beatPlaying != beat) {
+        // set the beat playing in redux store and component state
+        stopAllAudio();
+        dispatch(playback(beat));
+        setAudioMetadata();
+        setIsPlaying(true);
+        // restart beat and play
+        audio.currentTime = 0;
+        audio.play();
+      } else if (beatPlaying == beat && isPlaying) {
+        setIsPlaying(false);
+        audio.pause();
+      } else if (beatPlaying == beat && !isPlaying) {
+        dispatch(playback(beat));
+        setIsPlaying(true);
+        audio.play();
+      }
+    } else {
+      onClick(e);
+    }
+  };
+
   return (
-    <Row className={styles['row-container']}>
-      {isMobile ? null : buttonType === 'edit' ? (
-        <BeatEditModal beat={beat} />
-      ) : (
-        <BeatDownloadModal title={beat.title} artistName={beat.artistName} cdnKey={beat.audioKey} />
-      )}
-      <Row style={{ alignItems: 'center', marginRight: 'auto', paddingLeft: '1vw' }}>
-        <Image
-          src={`${cdnHostname}/${beat.artworkKey}`}
-          alt="album artwork"
-          preview={{
-            mask: <Image src={isPlaying ? pauseIcon : playIcon} preview={false} />,
-            visible: false,
-          }}
-          placeholder={<Image src={artworkLoading} width={isMobile ? 75 : 125} height={isMobile ? 75 : 125} />}
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null; // prevents looping
-            currentTarget.src = artworkLoading;
-          }}
-          width={isMobile ? 75 : 125}
-          height={isMobile ? 75 : 125}
-          onClick={(e) => {
-            if (isMobile) {
-              if (!audio) {
-                console.log('no audio ref detected');
-                stopAllAudio();
-              }
-              if (!audio.current) {
-                // stop all other beats that are playing
-                stopAllAudio();
-                audio.current = new Audio(`${cdnHostname}/${beat.audioKey}`);
-                audio.current.play();
-                setIsPlaying(true);
-              } else if (audio && isPlaying) {
-                audio.current.pause();
-                setIsPlaying(false);
-              } else {
-                audio.current.play();
-                setIsPlaying(true);
-              }
-            } else {
-              onClick(e);
-            }
-          }}
-          className={styles.artwork}
-        />
-        <div className={styles['text-container']}>
-          <h3
-            onClick={(e) => {
-              onClick(e);
+    <>
+      <Row className={styles['row-container']}>
+        {isMobile ? null : buttonType === 'edit' ? (
+          <BeatEditModal beat={beat} />
+        ) : (
+          <BeatDownloadModal title={beat.title} artistName={beat.artistName} cdnKey={beat.audioKey} />
+        )}
+        <Row style={{ alignItems: 'center', marginRight: 'auto', paddingLeft: '1vw' }}>
+          <Image
+            src={`${cdnHostname}/${beat.artworkKey}`}
+            alt="album artwork"
+            preview={{
+              mask: <Image src={isPlaying ? pauseIcon : playIcon} preview={false} />,
+              visible: false,
             }}
-            className={styles.title}
-          >
-            {beat.title}
-          </h3>
-          <h4 className={styles['info-text']}>
-            <a
-              style={{ color: artistNameColor }}
-              onMouseOver={() => {
-                setArtistNameColor('blue');
+            placeholder={<Image src={artworkLoading} width={isMobile ? 75 : 125} height={isMobile ? 75 : 125} />}
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null; // prevents looping
+              currentTarget.src = artworkLoading;
+            }}
+            width={isMobile ? 75 : 125}
+            height={isMobile ? 75 : 125}
+            // onClick={(e) => {
+            //   playBeat(e);
+            // }}
+            onClick={(e) => {
+              playBeat(e);
+            }}
+            className={styles.artwork}
+          />
+          <div className={styles['text-container']}>
+            <h3
+              onClick={(e) => {
+                playBeat(e);
               }}
-              onMouseLeave={() => {
-                setArtistNameColor('black');
-              }}
-              href={`/app/user/?id=${beat.artistId}`}
+              className={styles.title}
             >
-              {beat.artistName}
-            </a>{' '}
-            |{isMobile ? null : ` ${beat.genreTags[0]} |`} {beat.key}
-            {displayFlatOrSharp(beat.flatOrSharp)} {beat.majorOrMinor} {isMobile ? null : `| ${beat.tempo} bpm`}
-          </h4>
-        </div>
+              {beat.title}
+            </h3>
+            <h4 className={styles['info-text']}>
+              <a
+                style={{ color: artistNameColor }}
+                onMouseOver={() => {
+                  setArtistNameColor('blue');
+                }}
+                onMouseLeave={() => {
+                  setArtistNameColor('black');
+                }}
+                href={`/app/user/?id=${beat.artistId}`}
+              >
+                {beat.artistName}
+              </a>{' '}
+              |{isMobile ? null : ` ${beat.genreTags[0]} |`} {beat.key}
+              {displayFlatOrSharp(beat.flatOrSharp)} {beat.majorOrMinor} {isMobile ? null : `| ${beat.tempo} bpm`}
+            </h4>
+          </div>
+        </Row>
       </Row>
-    </Row>
+      <audio preload="auto" style={{ display: 'none' }} id={`audio-player-${beat.audioKey}`}>
+        <source src={`${cdnHostname}/${beat.audioKey}`} type="audio/mpeg" />
+      </audio>
+    </>
   );
 }
