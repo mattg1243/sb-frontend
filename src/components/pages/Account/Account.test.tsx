273eae65f6e8d200614a53f8359401b36324b44a
Matt Gallucci <mattgallucci97@gmail.com>
2023-06-14 10:32:03 -0700

    fixed test failing bug on Account.test

--- src/components/pages/Account/Account.test.tsx
+++ src/components/pages/Account/Account.test.tsx
@@ -2,10 +2,16 @@
 
 import { mount } from 'cypress/react18';
 import AccountPage from '.';
+import { Provider } from 'react-redux';
+import { store } from '../../../store';
 
 describe('Account.test.tsx', () => {
   beforeEach(() => {
-    mount(<AccountPage />);
+    mount(
+      <Provider store={store}>
+        <AccountPage />
+      </Provider>
+    );
   });
   it('contains page heading', () => {
     cy.get('h1.heading').contains('My Account');
