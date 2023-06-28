1cb0c153f697a12c2cc92dbd214c1428ce9ba743
Matt Gallucci <mattgallucci97@gmail.com>
2023-06-28 14:02:50 -0700

    added a search filter dropdown to dashboard page

---
+++ src/components/SearchFilter/index.tsx
@@ -0,0 +1,78 @@
+import { Dropdown, Button, MenuProps } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { Dispatch, SetStateAction } from 'react';
+import styles from './SearchFilter.module.css';
+
+export type SearchFilterOptions = 'Users' | 'Beats' | 'All';
+
+interface ISearchFilterProps {
+  currentSearchFilter: SearchFilterOptions;
+  setCurrentSearchFilter: Dispatch<SetStateAction<SearchFilterOptions>>;
+}
+
+export default function SearchFilter(props: ISearchFilterProps) {
+  const { currentSearchFilter, setCurrentSearchFilter } = props;
+
+  const filterMenuOptions: MenuProps['items'] = [
+    {
+      key: 'Beats',
+      label: (
+        <Button
+          type="ghost"
+          style={{ color: 'white' }}
+          onClick={() => {
+            if (currentSearchFilter !== 'Beats') {
+              setCurrentSearchFilter('Beats');
+            }
+          }}
+          id="rec-opt"
+        >
+          Beats
+        </Button>
+      ),
+    },
+    {
+      key: 'Users',
+      label: (
+        <Button
+          type="ghost"
+          style={{ color: 'white' }}
+          onClick={() => {
+            if (currentSearchFilter !== 'Users') {
+              setCurrentSearchFilter('Users');
+            }
+          }}
+          id="fol-opt"
+        >
+          Users
+        </Button>
+      ),
+    },
+    {
+      key: 'All',
+      label: (
+        <Button
+          type="ghost"
+          style={{ color: 'white' }}
+          onClick={() => {
+            if (currentSearchFilter !== 'All') {
+              setCurrentSearchFilter('All');
+            }
+          }}
+          id="fol-opt"
+        >
+          All
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <Dropdown menu={{ items: filterMenuOptions }}>
+      <Button type="ghost" className={styles['filter-btn']}>
+        {currentSearchFilter}
+        <DownOutlined />
+      </Button>
+    </Dropdown>
+  );
+}
