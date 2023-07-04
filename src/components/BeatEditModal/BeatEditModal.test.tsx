e6731b78d2ac240fcf25a85e6073c7613c182eac
Matt Gallucci <mattgallucci97@gmail.com>
2023-07-04 15:08:34 -0700

    removed testBeat to seperate dir

--- src/components/BeatEditModal/BeatEditModal.test.tsx
+++ src/components/BeatEditModal/BeatEditModal.test.tsx
@@ -2,29 +2,7 @@
 
 import { mount } from 'cypress/react18';
 import BeatEditModal from '.';
-import { Beat } from '../../types';
-
-// create a beat for testing
-const testBeat: Beat = JSON.parse(
-  JSON.stringify({
-    _id: '359dd4aa-8b2a-4196-9cf1-de4f991c55ba',
-    created_at: '2023-01-21T08:40:50.590Z',
-    updated_at: '2023-01-21T10:20:45.888Z',
-    title: 'Thriller',
-    artworkKey: 'images/1a29769b6c15669886384931327819de',
-    audioKey: 'beats/aa0cf1e6703d60ba22be47546066ac44',
-    artistId: 'c690083b-4597-478a-9e15-68c61789807c',
-    artistName: 'Montana Brown',
-    description: 'Hot ass beat clap',
-    tempo: 116,
-    key: 'A',
-    flatOrSharp: '',
-    majorOrMinor: 'major',
-    genreTags: '["Hip-Hop & Rap", "Funk", "Pop"]',
-    otherTags: null,
-    licensed: false,
-  })
-);
+import { testBeat } from '../../testFixtures/testBeat';
 
 describe('BeatEditModal.test.tsx', () => {
   beforeEach(() => {
---
+++ src/testFixtures/testBeat.ts
@@ -0,0 +1,24 @@
+import { type Beat } from '../types';
+
+export const testBeat: Beat = {
+  _id: '359dd4aa-8b2a-4196-9cf1-de4f991c55ba',
+  created_at: '2023-01-21T08:40:50.590Z',
+  updated_at: '2023-01-21T10:20:45.888Z',
+  title: 'Thriller',
+  artworkKey: 'images/1a29769b6c15669886384931327819de',
+  audioKey: 'beats/aa0cf1e6703d60ba22be47546066ac44',
+  artistId: 'c690083b-4597-478a-9e15-68c61789807c',
+  artistName: 'Montana Brown',
+  description: 'Hot ass beat clap',
+  tempo: 116,
+  streamsCount: 1920,
+  likesCount: 150,
+  downloadCount: 72,
+  hasStems: true,
+  key: 'A',
+  flatOrSharp: '',
+  majorOrMinor: 'major',
+  genreTags: ['Hip-Hop & Rap', 'Funk', 'Pop'],
+  otherTags: null,
+  licensed: false,
+};
