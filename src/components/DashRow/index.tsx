881ea43539d3555e0b4af815e6e61df0d3536a9f
Matt Gallucci <mattgallucci97@gmail.com>
2023-08-05 13:07:16 -0700

    console timing for cached request testing

--- src/components/DashRow/index.tsx
+++ src/components/DashRow/index.tsx
@@ -44,8 +44,13 @@ export default function DashRow(props: IBeatRowProps): JSX.Element {
   const [downloadCount, setDownloadCount] = useState<string>();
 
   useEffect(() => {
+    const reqTimeLabel = 'get like req time:';
+    console.time(reqTimeLabel);
     getUserLikesBeatReq(beat._id)
-      .then((res) => setLiked(res.data))
+      .then((res) => {
+        setLiked(res.data);
+        console.timeEnd(reqTimeLabel);
+      })
       .catch((err) => console.log(err));
     setDownloadCount(randomNumber(100, 1000).toLocaleString());
   }, []);
