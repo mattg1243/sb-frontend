55431517f09d007cad17760534839a59cb0946c5
Matt Gallucci <mattgallucci97@gmail.com>
2023-08-14 14:16:41 -0700

    added courier service websocket url for prod

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -3,10 +3,13 @@ const prodGatewayUrl = 'https://api.sweatshopbeats.com';
 const devSocketUrl = 'ws://localhost:4242';
 
 let gatewayUrl: string;
+let socketUrl: string;
+
+process.env.NODE_ENV === 'production' ? (socketUrl = 'https://courier.sweatshopbeats.com') : (socketUrl = devSocketUrl);
+export { socketUrl };
 
 process.env.NODE_ENV === 'production' ? (gatewayUrl = prodGatewayUrl) : (gatewayUrl = devGatewayUrl);
 
 export default gatewayUrl;
 
 export const cdnHostname = 'https://d3fulr0i8qqtgb.cloudfront.net';
-export const socketUrl = devSocketUrl;
