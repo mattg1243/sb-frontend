2183b1ea86b454c7485910c3c1272df9e3b37be9
Matt Gallucci <mattgallucci97@gmail.com>
2024-05-07 12:48:18 -0700

    added relevant text to subscription cards

--- src/components/pages/Subscription/index.tsx
+++ src/components/pages/Subscription/index.tsx
@@ -1,7 +1,6 @@
 import styles from './Subscription.module.css';
 import { Content } from 'antd/lib/layout/layout';
-import { Row, Button, Layout } from 'antd';
-import orangelogo from '../../../assets/orangelogo.png';
+import { Row, Layout, Menu, Image } from 'antd';
 import axios from 'axios';
 import gatewayUrl from '../../../config/routing';
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +10,8 @@ import StdCard from './StdCard';
 import PremCard from './PremCard';
 import Bottom from './Bottom';
 import Navbar from '../../Navbar/v2';
+import logo from '../../../assets/orangelogo.png';
+import { Header } from 'antd/es/layout/layout';
 
 // TDOD make a seperate SubscriptionCard component that takes price, name, and product description as props
 export default function Subscription() {
@@ -36,7 +37,46 @@ export default function Subscription() {
 
   return (
     <Layout>
-      <Navbar />
+      {window.innerWidth > 480 ? (
+        <Navbar />
+      ) : (
+        <Row
+          style={{
+            width: '100%',
+            margin: 0,
+            top: 0,
+            height: '75px',
+            background: 'black',
+            position: 'absolute',
+            zIndex: 1000,
+            boxShadow: '0px 5px rgb(232, 162, 21)',
+          }}
+          justify="center"
+        >
+          <Image
+            height="75px"
+            src={logo}
+            preview={false}
+            onClick={() => {
+              navigate('/app/dash');
+            }}
+          />
+        </Row>
+        // <Header style={{ width: '100%', margin: 0, top: 100, background: 'black', position: 'fixed' }}>
+        //   <Menu theme="dark" mode="horizontal">
+        //     <Menu.Item>
+        //       <Image
+        //         height="45px"
+        //         src={logo}
+        //         preview={false}
+        //         onClick={() => {
+        //           navigate('/app/dash');
+        //         }}
+        //       />
+        //     </Menu.Item>
+        //   </Menu>
+        // </Header>
+      )}
       <Content className={styles.content}>
         <h1 className={styles.headie}>Choose the subsciption that's right for you.</h1>
         <Row className={styles.subTiers}>
